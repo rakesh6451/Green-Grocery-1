e60e7ef7202a64347949280b87101c295c14396e
anirudhjak06 <anirudh.jakhotia6@gmail.com>
2021-12-11 22:45:19 +0530

    Update auth.js

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -3,7 +3,7 @@ const User = require("../models/User");
 const CryptoJS = require("crypto-js");
 const jwt = require("jsonwebtoken");
 
-//REGISTER
+//REGISTER mannnnn
 router.post("/register", async (req, res) => {
   const newUser = new User({
     username: req.body.username,
